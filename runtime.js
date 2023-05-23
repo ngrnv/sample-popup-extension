6cc43a3151d15ebcf06d50f2b3570e13bae752fc
ngrnv <al.nagornov@gmail.com>
2023-05-23 19:34:49 +0400

    logs

--- runtime.js
+++ runtime.js
@@ -1,14 +1,22 @@
 document.getElementById("clicker").addEventListener("click", onCLick);
 
-export function onCLick() {
+export async function onCLick() {
+  await chrome.runtime.sendMessage({ type: "log", data: 'onCLick' }); // only this one is logged in service worker
   chrome.tabs
     .create({
       url: "https://www.google.com/",
       active: false,
     })
     .then((tab) => {
+      // this never get called
       window.document.getElementById("output").innerHTML = tab.id
-      chrome.runtime.sendMessage({ type: "log", data: ['tab opened', tab.id] });
+      return chrome.runtime.sendMessage({ type: "log", data: ['tab opened', tab.id] });
     })
-    .catch((err) => (window.document.getElementById("output").innerHTML = err.message));
+    .catch((err) => {
+      // and this
+      window.document.getElementById("output").innerHTML = err.message;
+      return chrome.runtime.sendMessage({ type: "log", data: ['error', err.message] });
+    })
+    // and this
+    .finally(() => chrome.runtime.sendMessage({ type: "log", data: 'finally' }))
 }
